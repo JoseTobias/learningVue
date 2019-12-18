ac79b7d470195ec01342d3b835cb84d53d4ff2f1
JoseTobias <tobiassouza0gmail.com>
2019-12-18 11:06:14 -0300

    adding route dinamiccss

--- vue2/src/routes.js
+++ vue2/src/routes.js
@@ -3,9 +3,11 @@ import SingleGet from './components/SingleGet.vue'
 import All from './components/All.vue'
 import Input from './components/Input.vue'
 import TechTalk from '@/components/techTalk.vue'
+import DinamicCss from '@/components/DinamicCss.vue'
 
 export default [
     { path: '/', component: TechTalk },
+    { path: '/dinamiccss', component: DinamicCss },
     { path: '/all', component: All },
     { path: '/get', component: Get },
     { path: '/get/:id', component: SingleGet},
